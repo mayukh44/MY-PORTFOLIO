27eb62d72e00489e100cf837360ef9d8d959e632
mayukh44 <mayukhbiswas912@gmail.com>
2026-09-21 15:36:16 +0530

    no pic changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Toaster } from './components/ui/toaster.jsx'
 function App() {
 
   return (
-    <div className='select-none pointer-events-none'>
+    <div>
     <BrowserRouter>
       <Routes>
         <Route index element={<Home/>}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import { StartBackGround } from "../components/StarBackGround"
 import { ThemeToggle } from "../components/ThemeToggle"
 
 export const Home = () => {
-  return ( <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+  return ( <div className="min-h-screen bg-background text-foreground overflow-x-hidden select-none">
 
 
     { /*Theme Toggle (Dark/Light) */}
